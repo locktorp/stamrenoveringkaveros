bce367a8750903002ffa3f8cd9651af734c35b24
Christian <kc_andersson@hotmail.com>
2026-03-11 17:12:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, setDoc, onSnapshot, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDZY6BJsd1MkzHIY6gUkwFFXz43ACv1NAk",
-  authDomain: "stamrenovering-kaveros.firebaseapp.com",
-  projectId: "stamrenovering-kaveros",
-  storageBucket: "stamrenovering-kaveros.firebasestorage.app",
-  messagingSenderId: "974224372973",
-  appId: "1:974224372973:web:009e07621ecf4cbe5f88e4"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-let aptData = {}
-
 const ACTIVITY_GROUPS={
 "RIVNING":["Asbestsanering","Rivning","Slipning väggar","Håltagning","Städning"],
 "VVS":["Kassettmontage","Rör i badrum","Rör till kök/WC","Avloppstam kök","Blandare diskbänk","Gummi kassettbotten","Kassettfront monterad","Städning"],
@@ -34,44 +17,8 @@ const buildings={
 "Dirigentgatan 4":[[411,405,399,394],[412,406,400,395],[413,407,401,396],[414,408,402,397],[415,409,403,398],[416,410,404,null],[433,427,421,418],[434,428,422,419],[435,429,423,420],[436,430,424,null],[437,431,425,null],[438,432,426,417]]
 }
 
-function getData(){ return aptData }
-
-function saveData(d){
-aptData = d
-Object.entries(d).forEach(([id,data])=>{
-setDoc(doc(db,"apartments",id),data)
-})
-}
-
-onSnapshot(collection(db,"apartments"), (snapshot)=>{
-snapshot.forEach(docSnap=>{
-aptData[docSnap.id] = docSnap.data()
-})
-renderPlan()
-})
-
-
-
-const ACTIVITY_GROUPS={
-"RIVNING":["Asbestsanering","Rivning","Slipning väggar","Håltagning","Städning"],
-"VVS":["Kassettmontage","Rör i badrum","Rör till kök/WC","Avloppstam kök","Blandare diskbänk","Gummi kassettbotten","Kassettfront monterad","Städning"],
-"BYGG":["Formning Genomföringar","Fönstermontage","Torkutrustning utställd","LP50 tak","Gjutning Genomföringar","Stomme väggar","Gipsning slitsar","Stomme tak","Gipsning köksschakt","Ny dörromfattning","Torkutrustning bortplockad","Kylskåp tillbakaställt","Städning"],
-"KÖKSFÖRNYELSE":["Demontering","Målning stommar","Målning väggar","Montering stommar","Golvläggning","Kakel våtdel","Köksfläkt","Vitvaror","Belysning"],
-"EL":["Omtrådning","Kanalisation vägg och tak","IMD-Kanalisation i kassett","Installation IMD"],
-"MÅLNING":["Spackling tak","Spackling slitsar"],
-"KAKEL":["Golvavjämning","Tätskikt","Plattsättning","Fogning","Städning"],
-"KOMPLETTERING":["Montering dörr","Montering skåp","Montering sakvaror","Installation belysning","Mjukfogning","Montering porslin","Vatten på"]
-}
-
-const buildings={
-"Dirigentgatan 6":[[456,450,444,439],[457,451,445,440],[458,452,446,441],[459,453,447,442],[460,454,448,443],[461,455,449,null],[478,472,466,463],[479,473,467,464],[480,474,468,465],[481,475,469,null],[482,476,470,null],[483,477,471,462]],
-"Dirigentgatan 3":[[498,493,488,484],[499,494,489,485],[500,495,490,486],[501,496,491,487],[502,497,492,null],[523,517,511,null],[524,518,512,503],[519,513,507,504],[520,514,508,505],[521,515,509,506],[522,516,510,null]],
-"Dirigentgatan 1":[[539,534,529,525],[540,535,530,526],[541,536,531,527],[542,537,532,528],[543,538,533,null],[564,558,552,null],[565,559,553,544],[560,554,548,545],[561,555,549,546],[562,556,550,547],[563,557,551,null]],
-"Dirigentgatan 4":[[411,405,399,394],[412,406,400,395],[413,407,401,396],[414,408,402,397],[415,409,403,398],[416,410,404,null],[433,427,421,418],[434,428,422,419],[435,429,423,420],[436,430,424,null],[437,431,425,null],[438,432,426,417]]
-}
-
-")}
-
+function getData(){return JSON.parse(localStorage.getItem("aptData")||"{}")}
+function saveData(d){localStorage.setItem("aptData",JSON.stringify(d))}
 
 function allActivities(){
 let list=[]
