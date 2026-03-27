befe9cdaaee6535a734b8738e27f2593371845bb
Christian <kc_andersson@hotmail.com>
2026-03-27 16:02:33 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -457,12 +457,3 @@ renderPlan()
 }
 
 }
-function toggleMenu(){
-const menu = document.getElementById("mobileMenu")
-menu.style.display = menu.style.display === "block" ? "none" : "block"
-}
-
-function go(page){
-document.getElementById("mobileMenu").style.display="none"
-window.location.href = page
-}
