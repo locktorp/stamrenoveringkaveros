ebe4fcf0dd01ca824102a4e63e65b069b32a2325
Christian <kc_andersson@hotmail.com>
2026-04-21 08:29:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -137,7 +137,15 @@ stams.forEach((stam,i)=>{
 
 const col=document.createElement("div")
 col.className="stam"
-col.innerHTML="<div class='stam-title'>Stam "+(i+1)+"</div>"
+const stamName = "Stam " + (i+1)
+
+/* 🔥 HÄMTA AKTIVITET */
+const activity = getActivity(name, stamName)
+
+col.innerHTML = `
+  <div class='stam-title'>${stamName}</div>
+  <div class='stam-activity'>${activity}</div>
+`
 
 stam.forEach(num=>{
 
